13ce4989c520520a9f2454e892a8431af2220d77
c0a2408699 <c0a2408699@edu.teu.ac.jp>
2025-04-22 15:17:41 +0900

    ３限課題

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -16,7 +17,29 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+    bb_img=pg.Surface((20, 20))
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct=random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bb_rct=bb_img.get_rect()
+    vx=5
+    vy=5
+    DELTA={
+            pg.K_UP:(0,-5),
+            pg.K_DOWN:(0,5),
+            pg.K_LEFT:(-5,0),
+            pg.K_RIGHT:(5,0)
+        }
+    def check_bound(rct:pg.Rect) -> tuple[bool,bool]: 
+        yoko,tate=True,True
+        if 0 > rct.left or rct.right > WIDTH:
+            yoko=False
+        if 0 > rct.top or rct.bottom > HEIGHT:
+            tate=False
+        return yoko,tate
+
     while True:
+        
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
@@ -24,19 +47,27 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)
+        if kk_rct.colliderect(bb_rct):
+            return
+        if not check_bound(bb_rct)[0]:
+            vx=vx*-1
+        if not check_bound(bb_rct)[1]:
+            vy=vy*-1
+        
+
         pg.display.update()
         tmr += 1
-        clock.tick(50)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
